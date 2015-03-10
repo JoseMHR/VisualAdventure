4c5611c3b318a33a6de315cda872fc247bd82df4
Jose Manuel Heredia Reyes <Jose.manuel.reyes.1986@gmail.com>
2015-03-10 12:45:02 +0100

    Upload Event Heladeria and Iglesia

---
+++ VisualAdventure Source Code/src/taos/timedestiny/eventos/Heladeria.java
@@ -0,0 +1,53 @@
+package taos.timedestiny.eventos;
+
+import taos.timedestiny.bases.EventoBase;
+import android.os.Bundle;
+import android.view.View;
+
+public class Heladeria extends EventoBase {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+	// ######### Load Escena #################
+	public void loadScene(int escena) {
+		switch (escena) {
+			case 1:
+				setBGM("Espacio");
+				setFondo("heladeria1");
+				setPjA();
+				setPjB();
+
+				longText = 1;
+				text[0] = "Estas en " + protagonista.getLocalizacion();
+				setTexto();
+
+				break;
+			case 2:
+
+				break;
+		}
+	}
+
+	public void onOpcion1(View view) {
+		disableOp();
+	}
+
+	public void onOpcion2(View view) {
+		disableOp();
+	}
+
+	public void onOpcion3(View view) {
+		disableOp();
+	}
+
+	public void onOpcion4(View view) {
+		disableOp();
+	}
+
+	public void onOpcion5(View view) {
+		disableOp();
+	}
+}
